f76a394850a5339d55f2f3b359e3e4fc5a59fc55
PerfectPan <3207113562@qq.com>
2020-02-17 12:00:57 +0800

    feat: add animation on abstract box && extract abstract box component

---
+++ src/components/box.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react"
+import { Link } from "gatsby"
+import { rhythm } from "../utils/typography"
+import "./box-style.css"
+
+class Box extends Component {
+  render() {
+    const { node } = this.props
+    const title = node.frontmatter.title || node.fields.slug
+    const h3Style = {
+        marginBottom: rhythm(1 / 2),
+        marginTop: 0,
+    }
+
+    return (
+      <div className="box-outside">
+        <h3 style={h3Style}>
+          <Link style={{ boxShadow: `none` }} to={`blog${node.fields.slug}`}>
+            {title}
+          </Link>
+        </h3>
+        <p
+          style={{ marginBottom: rhythm(1 / 4)}}
+          dangerouslySetInnerHTML={{
+            __html: node.frontmatter.description || node.excerpt,
+          }}
+        />
+        <small>Written on {node.frontmatter.date}</small>
+      </div>
+    )
+  }
+}
+
+export default Box
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,9 +4,8 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
 import Button from "../components/button"
-
+import Box from "../components/box"
 class Blog extends React.Component {
   render() {
     const { data } = this.props
@@ -19,29 +18,7 @@ class Blog extends React.Component {
         <Bio />
         <div style={{ margin: "20px 0 40px" }}>
           {posts.map(({ node }) => {
-            const title = node.frontmatter.title || node.fields.slug
-            return (
-              <div key={node.fields.slug}>
-                <h3
-                  style={{
-                    marginBottom: rhythm(1 / 4),
-                  }}
-                >
-                  <Link
-                    style={{ boxShadow: `none` }}
-                    to={`blog${node.fields.slug}`}
-                  >
-                    {title}
-                  </Link>
-                </h3>
-                <small>{node.frontmatter.date}</small>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
-              </div>
-            )
+            return <Box key={node.fields.slug} node={node}/>
           })}
         </div>
         <Link to="/">
