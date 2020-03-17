3095ad1dbde753b01f2199d793715c98bbafd8ca
PerfectPan <3207113562@qq.com>
2020-03-17 23:33:30 +0800

    feat: update cms config.yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,20 @@ module.exports = {
     },
   },
   plugins: [
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-katex`,
+            options: {
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`
+            }
+          },
+        ]
+      }
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
