4ab2d2903b4d815c9909895ec66e1ac97b413c2e
PerfectPan <24316656+PerfectPan@users.noreply.github.com>
2026-09-01 11:38:53 +0800

    fix(web): replace the unrecognizable figlet with the legible standard-font perfectpan (.dev → .org)

--- apps/web/src/skins/terminal/home.tsx
+++ apps/web/src/skins/terminal/home.tsx
@@ -1,10 +1,11 @@
 import { Link } from '@tanstack/react-router';
 
-const FIGLET = `  ____              _   _        _   ____  __  __ ____
- |  _ \\ _   _  __ _| |_| |__    / \\ |  _ \\|  \\/  |  _ \\
- | |_) | | | |/ _\` | __| '_ \\  / _ \\| |_) | |\\/| | |_) |
- |  __/| |_| | (_| | |_| | | |/ ___ \\  __/| |  | |  __/
- |_|    \\__,_|\\__,_|\\__|_| |_/_/   \\_\\_|  |_|  |_|_|   `;
+const FIGLET = `                   __           _
+  _ __   ___ _ __ / _| ___  ___| |_ _ __   __ _ _ __
+ | '_  / _  '__| |_ / _ / __| __| '_  / _\` | '_ \
+ | |_) |  __/ |  |  _|  __/ (__| |_| |_) | (_| | | | |
+ | .__/ ___|_|  |_|  ___|______|__| .__/ __,_|_| |_|
+ |_|                               |_|`;
 
 export function TerminalHomePage() {
   return (
@@ -20,7 +21,7 @@ export function TerminalHomePage() {
         <div className='th-hero-name'>PerfectPan</div>
         <pre className='th-figlet' aria-hidden='true'>
           {FIGLET}
-          <b>.dev</b>
+          <b>.org</b>
         </pre>
       </div>
       <div className='th-home-links'>
